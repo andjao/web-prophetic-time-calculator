ec79ad8e2091177350e04999ebded61244a5b257
andjao <ads.marchi@gmail.com>
2020-03-24 17:15:37 -0300

    remove console.log

--- script/textsLanguage.js
+++ script/textsLanguage.js
@@ -1,8 +1,7 @@
 window.onload = function () {
     const pathLanguage = "./languages/"
     const navLang = navigator.language.substring(0,2);
-    console.log(navLang.indexOf('pt'));
-    console.log(navLang + " " + navLang.indexOf('pt') + " " + navLang.indexOf('en') + " " + navLang.indexOf('es'));
+    
     if (navLang.indexOf('pt') > -1 || navLang.indexOf('en') > -1 || navLang.indexOf('es') > -1) {
         setLang(pathLanguage + navLang + ".json");
     } else {
