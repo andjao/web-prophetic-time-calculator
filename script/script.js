76d024559c14284dc8b19f970f3e41af56d41dca
andjao <ads.marchi@gmail.com>
2020-03-26 08:59:44 -0300

    arround

--- script/script.js
+++ script/script.js
@@ -37,7 +37,7 @@ function calcTime(type) {
             tYears = value / 24 / 60 / 60;
             break;
         case 'minutes':
-            type = value < 2 ? texts.tSingular.tMinute : texts.tPlural.tSeconds;
+            type = value < 2 ? texts.tSingular.tMinute : texts.tPlural.tMinutes;
             tYears = value / 24 / 60;
             break;
         case 'hours':
@@ -61,23 +61,23 @@ function calcTime(type) {
             tYears = value * 365;
     }
 
-    const tMonths = tYears * 12;
-    const tWeeks = tYears * 52;
-    const tDays = tYears * 365;
-    const tHours = tDays * 24;
-    const tMinutes = tHours * 60;
-    const tSeconds = tMinutes * 60;
+    const tMonths = Math.floor(tYears * 12);
+    const tWeeks = Math.floor(tYears * 52);
+    const tDays = Math.floor(tYears * 365);
+    const tHours = Math.floor(tDays * 24);
+    const tMinutes = Math.floor(tHours * 60);
+    const tSeconds = Math.floor(tMinutes * 60);
 
     const print = value + " "
         + type + " "
         + (value < 2 ? texts.tSingular.tLiteral : texts.tPlural.tLiterals)
         + " equivale à:<br><br>"
-        + Math.floor(tSeconds) + " " + (tSeconds < 2 ? texts.tSingular.tSecond : texts.tPlural.tSeconds) + " " + (tSeconds < 2 ? texts.tSingular.tPropheticM : texts.tPlural.tPropheticsM) + ".<br>"
-        + Math.floor(tMinutes) + " " + (tMinutes < 2 ? texts.tSingular.tMinute : texts.tPlural.tMinutes) + " " + (tMinutes < 2 ? texts.tSingular.tPropheticM : texts.tPlural.tPropheticsM) + ".<br>"
-        + Math.floor(tHours) + " " + (tHours < 2 ? texts.tSingular.tHour : texts.tPlural.tHours) + " " + (tHours < 2 ? texts.tSingular.tPropheticM : texts.tPlural.tPropheticsM) + ".<br>"
-        + Math.floor(tDays) + " " + (tDays < 2 ? texts.tSingular.tDay : texts.tPlural.tDays) + " " + (tDays < 2 ? texts.tSingular.tPropheticM : texts.tPlural.tPropheticsM) + ".<br>"
-        + Math.floor(tWeeks) + " " + (tWeeks < 2 ? texts.tSingular.tWeek : texts.tPlural.tWeeks) + " " + (tWeeks < 2 ? texts.tSingular.tPropheticF : texts.tPlural.tPropheticsF) + ".<br>"
-        + Math.floor(tMonths) + " " + (tMonths < 2 ? texts.tSingular.tMonth : texts.tPlural.tMonths) + " " + (tMonths < 2 ? texts.tSingular.tPropheticM : texts.tPlural.tPropheticsM) + ".<br>"
+        + tSeconds + " " + (tSeconds < 2 ? texts.tSingular.tSecond : texts.tPlural.tSeconds) + " " + (tSeconds < 2 ? texts.tSingular.tPropheticM : texts.tPlural.tPropheticsM) + ".<br>"
+        + tMinutes + " " + (tMinutes < 2 ? texts.tSingular.tMinute : texts.tPlural.tMinutes) + " " + (tMinutes < 2 ? texts.tSingular.tPropheticM : texts.tPlural.tPropheticsM) + ".<br>"
+        + tHours + " " + (tHours < 2 ? texts.tSingular.tHour : texts.tPlural.tHours) + " " + (tHours < 2 ? texts.tSingular.tPropheticM : texts.tPlural.tPropheticsM) + ".<br>"
+        + tDays + " " + (tDays < 2 ? texts.tSingular.tDay : texts.tPlural.tDays) + " " + (tDays < 2 ? texts.tSingular.tPropheticM : texts.tPlural.tPropheticsM) + ".<br>"
+        + tWeeks + " " + (tWeeks < 2 ? texts.tSingular.tWeek : texts.tPlural.tWeeks) + " " + (tWeeks < 2 ? texts.tSingular.tPropheticF : texts.tPlural.tPropheticsF) + ".<br>"
+        + tMonths + " " + (tMonths < 2 ? texts.tSingular.tMonth : texts.tPlural.tMonths) + " " + (tMonths < 2 ? texts.tSingular.tPropheticM : texts.tPlural.tPropheticsM) + ".<br>"
         + Math.floor(tYears) + " " + (tYears < 2 ? texts.tSingular.tYear : texts.tPlural.tYears) + " " + (tYears < 2 ? texts.tSingular.tPropheticM : texts.tPlural.tPropheticsM);
 
     document.getElementById("result").innerHTML = print;
